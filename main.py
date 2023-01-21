6a6f70f3496790c07719d705ae5dad0c899febc3
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-01-21 09:36:36 +0300

    added delete post using pop for deletion

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p ["id"] == id:
+            return i
+
+
+
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to my api dev journey for 22 days"}
@@ -35,9 +44,9 @@ def get_posts():
     return {"data": my_posts}
 
 
-#retrieving multiple posts
+#creating a post
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict= post.dict()
     post_dict["id"] = randrange(0, 1000000)
@@ -55,5 +64,18 @@ def get_post(id: int, response: Response):
         #response.status_code = status.HTTP_404_NOT_FOUND
         #return {"message": f"post with id: {id} was not found"}
     return {"post_detail": Post}
+
+
+#deleting post
+#find the endex in the array that has required ID
+#my_post.pop(index)-- for deleting
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # title str, content str, category, bool published
